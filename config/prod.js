98013a049b59c3058c53563fc89b41ace942a162
csivana <chunduru.siva-naga-sai@adp.com>
2021-06-09 16:05:46 +0530

    first commit

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGOURI : "mongodb+srv://<username>:<password>@cluster0.vqoq7.mongodb.net/SocialApp?retryWrites=true&w=majority",
+    JWT_SECRET : "supersecretpassword",
+    SENDGRID_API : process.env.SENDGRID_API,
+    EMAIL_LINK : "www.gamergram.com"
+}
